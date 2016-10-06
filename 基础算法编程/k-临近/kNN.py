ad3b7583e51757244104f08be67980bbf7bfb81d
Liutian <15928551829@163.com>
2016-10-06 08:49:47 +0800

    2016/10/6 add k-临近

---
+++ 基础算法编程/k-临近/kNN.py
@@ -0,0 +1,42 @@
+from numpy import *
+import operator
+
+def file2matrix(filename):
+    fr=open(filename,'r+')
+    arrayOLines=fr.readlines()
+    numberOfLines=len(arrayOLines)
+    returnMat=zeros((numberOfLines,3))
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        line=line.strip()
+        listFromLine=line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index=index+1
+
+#k-means算法（利用欧式距离计算）
+def classfy0(inX,dataSet,labels,k):
+    dataSetSize=dataSet.shape[0]
+    diffMat=tile(inX,(dataSetSize,1))-dataSet
+    sqDiffMat=diffMat**2
+    sqDistances=sqDiffMat.sum(axis=-1)
+    distances=sqDistances**0.5
+    sortedDistIndicies=distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel=labels(sortedDistIndicies)
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter[1],reverse=true)
+    return sortedClassCount[0][0]
+
+#归一化数据
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minVals,(m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
